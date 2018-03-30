2e39c4310d001aca20b816c9e5f1f180df8fef6a
Calcey <calcey@Calceys-iMac-2.local>
2018-03-30 09:42:39 +0530

    Completed save function

--- Utility Converter/Common/UserDefaultHandler.swift
+++ Utility Converter/Common/UserDefaultHandler.swift
@@ -9,22 +9,32 @@
 import Foundation
 class  UserDefaultsHandler{
     let defaults = UserDefaults.standard
-    var history = [String]()
     
     func  save(data:String, key : String) {
-       let array =  defaults.array(forKey: key)
-       if (array != nil && history.isEmpty){
-            for records in array! {
-                history.append(records as! String)
-            }
-       }
+       var history : [String] = loadHistory(key: <#T##String#>)
        history.append(data)
-        
        defaults.set(history, forKey: key)
        if history.count > 5 {
          history.remove(at: 0) //This is to remove older records
        }
        defaults.set(history, forKey: key)
+    }
+    
+    func  getData(key:String) -> [String] {
+        var history : [String] = loadHistory(key: <#T##String#>);
+        history.reverse();
+        return history;        
+    }
+    
+    func  loadHistory(key: String) -> [String]{
+        var history = [String]()
+        let array =  defaults.array(forKey: key)
+        if (array != nil && history.isEmpty){
+            for records in array! {
+                history.append(records as! String)
+            }
+        }
+        return history;
     }
     
 }
